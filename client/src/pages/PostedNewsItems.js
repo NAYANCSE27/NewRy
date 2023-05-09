1c0fb8c483bc59213a9b187135773541109d0fa1
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-05-09 19:29:40 +0600

    added basic structure of the newry project

---
+++ client/src/pages/PostedNewsItems.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PostedNewsItems() {
+    return (
+        <div>
+            <h1>Posted News Items</h1>
+        </div>
+    );
+}
+
+export default PostedNewsItems;
