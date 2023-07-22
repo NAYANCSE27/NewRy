5862fa74dced035659973bedd0e3ad1dc49dfab7
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-07-22 17:16:51 +0600

    remove all older file

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
-import "./App.css";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import AddNews from "./pages/AddNews";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/add" element={<AddNews />} />
-        </Routes>
-      </BrowserRouter>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
