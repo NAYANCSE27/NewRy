82afac664082f2cd081a356a2d1a1df9ad30ece4
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-07-22 22:27:10 +0600

    basic registration done with mongodb connection

--- api/index.js
+++ api/index.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const { mongoose } = require("mongoose");
+const bcrypt = require("bcrypt");
+const User = require("./models/User");
 
 const app = express();
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/register", (req, res) => {
+mongoose.connect(
+  "mongodb+srv://node101:node101@node101.6h8jzwm.mongodb.net/blog?retryWrites=true&w=majority"
+);
+
+app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  res.json({ requestData: { username, password } });
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 app.listen(4000, () => {
