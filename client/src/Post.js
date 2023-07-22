d9c149154790198f6ac72114fae3935cab469430
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-07-22 19:46:20 +0600

    added basic staructure of the project

---
+++ client/src/Post.js
@@ -0,0 +1,23 @@
+function Post() {
+  return (
+    <div className="post">
+      <div className="image">
+        <img src="https://unsplash.com/photos/3ZUsNJhi_Ik"></img>
+      </div>
+      <div className="texts">
+        <h2>Full house battery backup with the new battery</h2>
+        <p className="info">
+          <a className="author">Mehadi Hasan</a>
+          <time>Feb 12, 2023</time>
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          exercitationem, quod tempora accusamus exercitationem. Quisquam,
+          quibusdam. Quisquam, quibusdam.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
